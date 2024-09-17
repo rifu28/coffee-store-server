ffcb3ca0bb871eb8f529bc91ae2ccb98c20dbc6f
rifu28 <rifatmeumeu28@gmail.com>
2024-09-17 20:07:07 +0600

    finish this

--- index.js
+++ index.js
@@ -47,6 +47,28 @@ async function run() {
       res.send(result);
     });
 
+    app.put(
+      "/coffee/:id",
+      async(req, (res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const updatedCoffee = req.body;
+        const coffee = {
+          $set: {
+            name: updatedCoffee.name,
+            quantity: updatedCoffee.quantity,
+            supplier: updatedCoffee.supplier,
+            taste: updatedCoffee.taste,
+            details: updatedCoffee.details,
+            photo: updatedCoffee.photo,
+          },
+        };
+        const result = coffeeCollection.updateOne(filter, coffee, options);
+        res.send(result);
+      })
+    );
+
     app.delete("/coffee:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
